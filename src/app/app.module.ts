dc7d61c9ebb4b97a4586bfa1656799f605ffd1c1
Mgonzal3z <upgrade_tech@hotmail.com>
2023-09-09 19:39:47 -0500

    update 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InfoComponent } from './info/info.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { XpComponent } from './xp/xp.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InfoComponent,
+    ProjectsComponent,
+    XpComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
