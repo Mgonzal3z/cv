844f62f9d06db2c19688e7b209e3c283d9152dc4
Mgonzal3z <upgrade_tech@hotmail.com>
2023-09-28 00:20:32 -0500

    update 8 light-mode

--- src/app/header-mobile/header-mobile.component.ts
+++ src/app/header-mobile/header-mobile.component.ts
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const headerBtn = document.querySelector('.header__bars') as HTMLElement;
   const mobileNav = document.querySelector('.mobile-nav') as HTMLElement;
+  const mobileLinks = document.querySelectorAll('.mobile-nav__link');
 
   let isMobileNavOpen = false;
 
@@ -26,6 +27,37 @@ document.addEventListener('DOMContentLoaded', () => {
       document.body.style.overflowY = 'auto';
     }
 
+    mobileLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        isMobileNavOpen = false;
+        mobileNav.style.display = 'none';
+        document.body.style.overflowY = 'auto';
+      })
+    });
+
   });
+
+  const themeToggleBtns = document.querySelectorAll('#theme-toggle');
+
+  // estado
+  const theme = localStorage.getItem('theme');
+
+  // carga
+  theme && document.body.classList.add(theme);
+
+  // handler
+  const handleThemeToggle = () => {
+    document.body.classList.toggle('light-mode');
+    if(document.body.classList.contains('light-mode')){
+      localStorage.setItem('theme', 'light-mode');
+    }else{
+      localStorage.removeItem('theme');
+    }
+  }
+  //evento
+  themeToggleBtns.forEach(btn => {
+    btn.addEventListener('click', handleThemeToggle)
+  });
+
 });
 
